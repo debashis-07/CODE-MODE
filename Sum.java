346f84346f8f4689d5a465c1b0983f43296937a8
debashis-07 <debashissahoo07.10.2007@gmail.com>
2026-01-12 04:37:37 +0530

    Sum of Digit

---
+++ Sum.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Sum {
+    
+  public static void main(String[] args){
+    Scanner sc=new Scanner(System.in);
+    System.out.print("Enter a number: ");
+    int num=sc.nextInt();
+    int sum=(num%10)+((num/10)%10)+(num/100);
+    System.out.println("Sum of the digit of the number "+num+" is "+sum);
+    sc.close();
+  }
+}
+
